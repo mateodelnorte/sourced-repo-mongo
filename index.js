7390aba83aff8459eac14644a487010daf2a7749
Matt Walters <matt.walters@electronifie.com>
2014-12-04 16:46:24 -0500

    rearrange / refactoring

--- index.js
+++ index.js
@@ -36,6 +36,38 @@ function Repository (entityType, indices) {
 
 util.inherits(Repository, EventEmitter);
 
+Repository.prototype.commit = function commit (entity, cb) {
+  var self = this;
+  log('committing %s for id %s', this.entityType.name, entity.id);
+  this.initialized.done(function () {
+    self._commitSnapshots(entity).then(function _afterCommitSnapshots () {
+      self._commitEvents(entity).then(function _afterCommitEvents () {
+        self._emitEvents(entity).then(function _afterEmitEvents () {
+          return cb();
+        });
+      }).catch(cb);
+    }).catch(cb);
+  });
+};
+
+Repository.prototype.commitAll = function commit (entities, cb) {
+  var self = this;
+  log('committing %s for id %j', this.entityType.name, _.pluck(entities, 'id'));
+  this.initialized.done(function () {
+    self._commitAllSnapshots(entities).then(function _afterCommitSnapshots () {
+      self._commitAllEvents(entities).then(function _afterCommitEvents () {
+        var promises = [];
+        entities.forEach(function (entity) {
+          promises.push(self._emitEvents(entity));
+        });
+        Promise.all(promises).then(function _afterEmitAllEvents () {
+          return cb();
+        });
+      }).catch(cb);
+    }).catch(cb);
+  });
+};
+
 Repository.prototype.get = function get (id, cb) {
   var self = this;
   log('getting %s for id %s', this.entityType.name, id);
@@ -53,74 +85,10 @@ Repository.prototype.get = function get (id, cb) {
           .toArray(function (err, events) {
             if (err) return cb(err);
             if (snapshot) delete snapshot._id;
-            var entity = self.deserialize(id, snapshot, events);
+            var entity = self._deserialize(id, snapshot, events);
             return cb(null, entity);
           });
     });
-  });
-};
-
-Repository.prototype._getAllSnapshots = function _getAllSnapshots (ids) {
-  var self = this;
-  return new Promise(function (resolve, reject) {
-    var match = { $match: { id: { $in: ids } } };
-    var group = { $group: { _id: '$id', snapshotVersion: { $last: '$snapshotVersion' } } };    
-    self.snapshots.aggregate([match, group], function (err, idVersionPairs) {
-      if (err) return reject(err);
-      var criteria = {};
-      if (idVersionPairs.length === 0) {
-        return resolve([]);
-      } else if (idVersionPairs.length === 1) {
-        criteria = { id: idVersionPairs[0]._id };
-      } else {
-        criteria.$or = [];
-        idVersionPairs.forEach(function (pair) {
-          var cri = { id: pair._id, snapshotVersion: pair.snapshotVersion };
-          criteria.$or.push(cri);
-        });
-      }
-      self.snapshots
-        .find(criteria)
-        .toArray(function (err, snapshots) {
-          if (err) reject(err);
-          resolve(snapshots);
-        });
-    });
-  });
-};
-
-Repository.prototype._getAllEvents = function _getAllEvents (ids, snapshots) {
-  var self = this;
-  return new Promise(function (resolve, reject) {
-    var criteria = { $or: [] };
-    ids.forEach(function (id) {
-      var snapshot;
-      if ( ! (snapshot = _.find(snapshots, function (snapshot) {
-        return id === snapshot.id;
-      }))) {
-        criteria.$or.push({ id: id });
-      } else {
-        criteria.$or.push({ id: snapshot.id, version: { $gt: snapshot.snapshotVersion } });
-      }
-    });
-    self.events.find(criteria)
-      .sort({ id: 1, version: 1 })
-      .toArray(function (err, events) {
-        if (err) return reject(err);
-        var results = [];
-        ids.forEach(function (id) {
-          var snapshot = _.find(snapshots, function (snapshot) {
-            return snapshot.id === id;
-          });
-          if (snapshot) delete snapshot._id;
-          var evnts = _.filter(events, function (event) {
-            return event.id === id;
-          });
-          var entity = self.deserialize(id, snapshot, evnts);
-          results.push(entity);
-        });
-        return resolve(results);
-      });
   });
 };
 
@@ -187,6 +155,7 @@ Repository.prototype._commitAllEvents = function _commitEvents (entities) {
   });
 };
 
+
 Repository.prototype._commitSnapshots = function _commitSnapshots (entity) {
   var self = this;
   return new Promise(function (resolve, reject) {
@@ -226,6 +195,13 @@ Repository.prototype._commitAllSnapshots = function _commitAllSnapshots (entitie
   });
 };
 
+Repository.prototype._deserialize = function _deserialize (id, snapshot, events) {
+  log('deserializing %s entity ', this.entityType.name);
+  var entity = new this.entityType(snapshot, events);
+  entity.id = id;
+  return entity;
+};
+
 Repository.prototype._emitEvents = function _emitEvents (entity) {
   var self = this;
   return new Promise(function (resolve, reject) {
@@ -240,86 +216,68 @@ Repository.prototype._emitEvents = function _emitEvents (entity) {
   });
 };
 
-Repository.prototype.commit = function commit (entity, cb) {
+Repository.prototype._getAllSnapshots = function _getAllSnapshots (ids) {
   var self = this;
-  log('committing %s for id %s', this.entityType.name, entity.id);
-  this.initialized.done(function () {
-    self._commitSnapshots(entity).then(function _afterCommitSnapshots () {
-      self._commitEvents(entity).then(function _afterCommitEvents () {
-        self._emitEvents(entity).then(function _afterEmitEvents () {
-          return cb();
+  return new Promise(function (resolve, reject) {
+    var match = { $match: { id: { $in: ids } } };
+    var group = { $group: { _id: '$id', snapshotVersion: { $last: '$snapshotVersion' } } };    
+    self.snapshots.aggregate([match, group], function (err, idVersionPairs) {
+      if (err) return reject(err);
+      var criteria = {};
+      if (idVersionPairs.length === 0) {
+        return resolve([]);
+      } else if (idVersionPairs.length === 1) {
+        criteria = { id: idVersionPairs[0]._id };
+      } else {
+        criteria.$or = [];
+        idVersionPairs.forEach(function (pair) {
+          var cri = { id: pair._id, snapshotVersion: pair.snapshotVersion };
+          criteria.$or.push(cri);
         });
-      }).catch(cb);
-    }).catch(cb);
+      }
+      self.snapshots
+        .find(criteria)
+        .toArray(function (err, snapshots) {
+          if (err) reject(err);
+          resolve(snapshots);
+        });
+    });
   });
 };
 
-Repository.prototype.commitAll = function commit (entities, cb) {
+Repository.prototype._getAllEvents = function _getAllEvents (ids, snapshots) {
   var self = this;
-  log('committing %s for id %j', this.entityType.name, _.pluck(entities, 'id'));
-  this.initialized.done(function () {
-    self._commitAllSnapshots(entities).then(function _afterCommitSnapshots () {
-      self._commitAllEvents(entities).then(function _afterCommitEvents () {
-        var promises = [];
-        entities.forEach(function (entity) {
-          promises.push(self._emitEvents(entity));
+  return new Promise(function (resolve, reject) {
+    var criteria = { $or: [] };
+    ids.forEach(function (id) {
+      var snapshot;
+      if ( ! (snapshot = _.find(snapshots, function (snapshot) {
+        return id === snapshot.id;
+      }))) {
+        criteria.$or.push({ id: id });
+      } else {
+        criteria.$or.push({ id: snapshot.id, version: { $gt: snapshot.snapshotVersion } });
+      }
+    });
+    self.events.find(criteria)
+      .sort({ id: 1, version: 1 })
+      .toArray(function (err, events) {
+        if (err) return reject(err);
+        var results = [];
+        ids.forEach(function (id) {
+          var snapshot = _.find(snapshots, function (snapshot) {
+            return snapshot.id === id;
+          });
+          if (snapshot) delete snapshot._id;
+          var evnts = _.filter(events, function (event) {
+            return event.id === id;
+          });
+          var entity = self._deserialize(id, snapshot, evnts);
+          results.push(entity);
         });
-        Promise.all(promises).then(function _afterEmitAllEvents () {
-          return cb();
-        });
-      }).catch(cb);
-    }).catch(cb);
-    // // save snapshots before saving events
-    // new Promise(function (resolve, reject) {
-    //   entities.forEach(function (entity) {
-
-    //   });
-    //   if (entity.version >= entity.snapshotVersion + 10) {
-    //     var snapshot = entity.snapshot();  
-    //     if (snapshot && snapshot._id) delete snapshot._id; // mongo will blow up if we try to insert multiple _id keys
-    //     self.snapshots.insert(snapshot, function (err) {
-    //       if (err) return reject(err);
-    //       log('committed %s.snapshot for id %s %j', self.entityType.name, entity.id, snapshot);
-    //       resolve(entity);
-    //     });
-    //   } else {
-    //     resolve(entity);
-    //   }  
-    // }).done(function (entity) {
-    //   function done () {
-    //     var eventsToEmit = entity.eventsToEmit;
-    //     entity.eventsToEmit = [];
-    //     eventsToEmit.forEach(function (eventToEmit) {
-    //       var args = Array.prototype.slice.call(eventToEmit);
-    //       self.entityType.prototype.emit.apply(entity, args);
-    //     });
-    //     log('emitted local events for id %s', entity.id);
-    //     return cb();
-    //   }
-    //   // when finished, save events
-    //   if (entity.newEvents.length === 0) return done();
-    //   var events = entity.newEvents;
-    //   events.forEach(function (event) {
-    //     if (event && event._id) delete event._id; // mongo will blow up if we try to insert multiple _id keys
-    //     self.indices.forEach(function (index) {
-    //       event[index] = entity[index];
-    //     });
-    //   });
-    //   self.events.insert(events, function (err) {
-    //     if (err) return cb(err);
-    //     log('committed %s.events for id %s', self.entityType.name, entity.id);
-    //     entity.newEvents = [];
-    //     return done();
-    //   });
-    // });
+        return resolve(results);
+      });
   });
-};
-
-Repository.prototype.deserialize = function deserialize (id, snapshot, events) {
-  log('deserializing %s entity ', this.entityType.name);
-  var entity = new this.entityType(snapshot, events);
-  entity.id = id;
-  return entity;
 };
 
 module.exports.Repository = Repository;
